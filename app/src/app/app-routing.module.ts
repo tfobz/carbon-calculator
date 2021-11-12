f22e73d362ff396dfb2e45f987bbf5d1fa12a4b9
hannest03 <hannesasd@gmail.com>
2021-11-12 19:56:03 +0100

    feat: added basic route

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UiComponent } from './ui/ui.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: UiComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
