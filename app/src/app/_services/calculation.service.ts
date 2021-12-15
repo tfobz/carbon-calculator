01948c6314a2bc5565c62136e5fdfe4d8f18884c
hannest03 <hannesasd@gmail.com>
2021-12-15 21:31:13 +0100

    feat: added calculations service

---
+++ app/src/app/_services/calculation.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Calculation } from '../emissionmodule/calculation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculationService {
+
+  private _calculations: Calculation[] = [];
+
+  get calculations(): Calculation[] {
+    return this._calculations;
+  }
+
+  load(){
+    const calculations: string | null = localStorage.getItem("calculations");
+    if(!calculations) return;
+
+    const calculationsData = JSON.parse(calculations);
+    if(!calculationsData) return;
+    if(!(calculationsData instanceof Array)) return;
+  
+    const temp: Calculation[] = [];
+    calculationsData.forEach((data: any) => {
+      temp.push(new Calculation().load(data));
+    });
+    this._calculations = temp;
+  }
+
+  save(){
+    const saveList: any = [];
+    this._calculations.forEach((calculation: Calculation) => {
+      saveList.push(calculation.save());
+    });
+    const json = JSON.stringify(saveList);
+    localStorage.setItem("calculations", json);
+  }
+
+}
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculationService } from './_services/calculation.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CalculationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
