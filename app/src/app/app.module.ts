7f00368ec61f7077eda883d61b16630cfcdc183f
iceeye2017 <johannes.h2003@gmail.com>
2022-01-17 11:46:55 +0100

    wip: Rewriting ui with angular material

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { EmissionItemComponent } from './emission-list/emission-item/emission-it
 import { EmissionListComponent } from './emission-list/emission-list.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ArrowComponent } from './navigation/arrow/arrow.component';
-import { CardComponent } from './card/card.component';
 import { CalculationListComponent } from './calculation-list/calculation-list.component';
 import { CalculationCardComponent } from './calculation-card/calculation-card.component';
 import { SpecificCalculationListComponent } from './specific-calculation-list/specific-calculation-list.component';
@@ -19,6 +18,11 @@ import { CalculationDiagramComponent } from './calculation-diagram/calculation-d
 
 import { NavigationService } from './shared/navigation.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -28,11 +32,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     EmissionListComponent,
     NavigationComponent,
     ArrowComponent,
-    CardComponent,
     CalculationListComponent,
     CalculationCardComponent,
     SpecificCalculationListComponent,
-	CalculationDiagramComponent
+	  CalculationDiagramComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +43,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts')
     }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
+
   ],
   providers: [
     NavigationService,
