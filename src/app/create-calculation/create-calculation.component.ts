ff74668bd33092520a5e99cd77d531e37384069f
Hannes <42945799+hannest03@users.noreply.github.com>
2022-06-11 11:44:49 +0200

    feat: added preset italy factors

--- src/app/create-calculation/create-calculation.component.ts
+++ src/app/create-calculation/create-calculation.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Calculation } from '../emissionmodule/calculation';
-import EmissionsManager from '../emissionmodule/emissions_manager';
+import { FactorManager } from '../emissionmodule/factor-manager';
+import { FactorProvider, PRESET_FACTORS_LIST } from '../emissionmodule/factor_provider';
 import { CalculationService } from '../_services/calculation.service';
 
 interface CalculationData{
-  name: string
+  name: string,
+  factors: string
 }
 
 @Component({
@@ -25,20 +27,24 @@ export class CreateCalculationComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    const data: CalculationData = { name: "" };
+    const data: CalculationData = { name: "", factors: PRESET_FACTORS_LIST[0] };
     
     this._createForm = this._fb.group({
-      name: [data.name, [ Validators.required, Validators.minLength(4) ]]
+      name: [data.name, [ Validators.required, Validators.minLength(4) ]],
+      factors: [ data.factors, Validators.required ]
     });
   }
 
   create(){
-    const data: CalculationData = { name: "" };
+    const data: CalculationData = { name: "", factors: "" };
     Object.assign(data, this._createForm.value);
 
     this._createForm.reset();
 
+    const factorManager: FactorManager = FactorProvider.get(data.factors);
+
     const calculation = new Calculation(data.name);
+    calculation.factorManager = factorManager;
     this._calculationService.addCalculation(calculation);
     this._calculationService.save();
     this._router.navigate(["emission"]);
@@ -46,6 +52,10 @@ export class CreateCalculationComponent implements OnInit {
 
   get createForm(): FormGroup{
     return this._createForm;
+  }
+
+  get PRESET_FACTORS_LIST(): string[] {
+    return PRESET_FACTORS_LIST;
   }
 
 }
