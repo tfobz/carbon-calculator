26ac470c54b1916780a0c9a0a0abf1f7daaca009
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-21 22:10:18 +0200

    Added comments and hint

--- src/app/emission-list/emission-list.component.ts
+++ src/app/emission-list/emission-list.component.ts
@@ -6,13 +6,18 @@ import { NavigationService } from '../shared/navigation.service';
 import { CalculationService } from '../_services/calculation.service';
 import { TranslationManagerService } from '../_services/translation-manager.service';
 
-
+// Defines where to search for styling and HTML of this page
 @Component({
   selector: 'app-emission-list',
   templateUrl: './emission-list.component.html',
   styleUrls: ['./emission-list.component.scss']
 })
-export class EmissionListComponent implements OnInit{
+
+// Is like a constructor for this page, defining variables and other things that can then be defined
+// in the HTML file
+export class EmissionListComponent implements OnInit {
+  // Custom variable to make Hints disappear once an entry got created
+  empty_hint = true;
   constructor(
     private navigation:NavigationService,
     private calculationService: CalculationService,
@@ -21,16 +26,25 @@ export class EmissionListComponent implements OnInit{
     private menuService:MenuService,
   ){}
 
+  // This function triggers every time you open this page
   ngOnInit(): void {
     this.translateService.get("emission").subscribe(translation => {
       this.navigation.changeMessage(translation);
     });
-    //Menu 
+
+    // Check if there is an emission item and change the visibility of the hints accordingly
+    // calculationService.calculations will retrieve nothing if there is no calculation, and something else if there is
+    if (this.calculationService.calculations.toString() && this.calculationService.calculations.toString().trim()) {
+      this.empty_hint = false;
+    }
+
+    // Defines the menu of the website
     this.translateService.getTranslation(this.translationManagerService.lang).subscribe(translations => {
       this.menuService.changeMenu([{icon:"settings", menuPointName: this.translationManagerService.getTranslation(translations, "settings"), link: `/emission/settings`}])
     });
   }
 
+  // Retrieves the list of all created calculations
   get calculations(): Calculation[] {
     this.calculationService.save()
     return this.calculationService.calculations;
