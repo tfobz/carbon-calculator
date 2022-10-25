e71a87cdb0550c13a8dff5206cafc26b5ecd367e
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-25 12:44:53 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -32,6 +32,12 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"]
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
