506146e2c6a48f4b4e61e8debc45e9354564fa28
Benji <50681275+Benji377@users.noreply.github.com>
2022-10-29 21:32:28 +0200

    Adding documentation

--- src/app/calculation-card/calculation-card.component.ts
+++ src/app/calculation-card/calculation-card.component.ts
@@ -3,6 +3,11 @@ import { Calculation } from '../emissionmodule/calculation';
 import { EmissionModule } from '../emissionmodule/emission-module';
 import { FactorManager } from '../emissionmodule/factor-manager';
 
+/**
+ * Defines the interface of the CalculationCardData.
+ * Every card has its own number, unit (for example m^3) and a result.
+ * The result is the calculated number from all modules in the card
+ */
 interface CalculationCardData{
   number: string,
   unit: string,
@@ -14,6 +19,15 @@ interface CalculationCardData{
   templateUrl: './calculation-card.component.html',
   styleUrls: ['./calculation-card.component.scss']
 })
+
+/**
+ * Defines the CalculationCard component.
+ * A Calculation card is the component that holds the amount of CO2 condumed by paper for example
+ * It always has a title, an amount and the result, whoch would be the amount of CO2 consumed
+ * To see one you need to do the following:
+ * 
+ * Create a new entry > click on it > click on the plus > add an entry > CalculationCard
+ */
 export class CalculationCardComponent implements OnInit {
 
   @Input() edit: boolean = true;
@@ -23,6 +37,10 @@ export class CalculationCardComponent implements OnInit {
   
   ngOnInit(): void {}
 
+  /**
+   * Is like a toString() method
+   * @returns Every variable saved in this class, or unknown if not set
+   */
   get data(): CalculationCardData[] {
     return [{ number: this.module?.number as unknown as string, unit: this.module?.unit, result: this.module?.calculate(this.factorManager).toFixed(2) as unknown as string }];
   }
